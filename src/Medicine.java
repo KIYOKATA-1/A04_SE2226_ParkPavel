20085840315ba25581992d359eb1f0feb43580ec
Me <park.pavel04@gmail.com>
2023-10-29 08:54:31 +0600

    реализация интерфейса для медикаментов

--- src/Medicine.java
+++ src/Medicine.java
@@ -1,3 +1,12 @@
 public class Medicine implements Product{
 
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,4 @@
 public interface Product {
+    String getName();
+    double getPrice();
 }
