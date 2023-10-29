2b3fe915ba795fe68eaa6de5e736aac494b1caa3
Me <park.pavel04@gmail.com>
2023-10-29 09:53:20 +0600

    создание класса который реализует наблюдателя и фабричный метод

---
+++ src/PharmHealth.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+public class PharmHealth {
+    private List<Observer> observers = new ArrayList<>();
+    private Pharm pharm;
+
+    public void setPharm(Pharm pharm){
+        this.pharm = pharm;
+    }
+
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer){
+        observers.remove(observer);
+    }
+
+    public void addProduct(String name, double price){
+        Product product = Pharm.createProduct(name,price);
+        notifyObservers(product);
+    }
+
+    private void notifyObservers(Product product) {
+        for (Observer observer : observers) {
+            observer.update(product);
+        }
+    }
+}
